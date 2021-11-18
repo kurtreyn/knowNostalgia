038849394072efe49444d41f47672bc8ffe0fe5d
Kurt Reynolds <kurtreynolds@Kurts-Air.mshome.net>
2021-11-18 12:54:03 -0500

    switched to bootstrap cdn for deployment unitl I get better understanding of creating dist

---
+++ gulpfileOLD.js
@@ -0,0 +1,26 @@
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+function style() {
+  return gulp
+    .src('./assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./assets/css'));
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./assets/sass/**/*.scss', style);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+    gulp.watch('./assets/scripts/**/*.js').on('change', browserSync.reload)
+}
+
+exports.style = style;
+exports.watch = watch;
